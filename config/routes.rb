8817f006ea865d6cfe89c948541823b325af4cd3
Colleen Garvey <colleenellengarvey@gmail.com>
2022-08-01 15:05:26 -0400

    user authentication, signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+
   root to: 'products#index'
+  
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -19,6 +21,13 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
 
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

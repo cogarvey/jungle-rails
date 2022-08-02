9a466af46a3db9444be17461737486fcecb7c296
Colleen Garvey <colleenellengarvey@gmail.com>
2022-08-01 20:04:41 -0400

    update product test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   describe 'Validations' do
+#     # validation examples here
+#     it 'should create a new user' do
+#       @user = User.create(name: 'Flowers', email: 'flowers@email.com', password: nil)
+#       expect(@user).to_not be_valid
+#     end
+#   end
+
+#   end
+# end
